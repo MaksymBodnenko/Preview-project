316cbab48b97aa20aad871921034658b51150665
Max <maximan@ukr.net>
2022-09-03 14:53:20 +0300

    my progFilms_1

--- myscript.js
+++ myscript.js
@@ -27,9 +27,6 @@ function start() {
 start();
 
 
-
-
-
 let personalMovieDB = {
 	count: numberOfFilms,
 	movies: {},
@@ -40,7 +37,7 @@ let personalMovieDB = {
 
 function rememberMyFilms() {
 	for (let i = 0; i < 2; i++) {
-		const a = prompt('Один из последних просмотренных фильмов?', ''),
+		const a = prompt('Один из последних просмотренных фильмов?', '').trim(),
 			b = prompt('На сколько оцените его?', '');
 
 		if (a != '' && a != null && a.length < 50 && b != null && b != '') {
